375069fb5c507227c5d23946735207ff0950abb1
Omar Ochoa <omar@ochoawebdesign.com>
2023-04-10 22:13:50 -0700

    Creates a function to randomly generate loot when clicking on Scavenge

---
+++ src/loot/itemList.js
@@ -0,0 +1,18 @@
+const items = [
+    {
+      name: "Sword of the Undead",
+      description: "A cursed sword that drains the life force of its wielder.",
+      value: "50 gold",
+    },
+    {
+      name: "Wand of Fireball",
+      description: "A wand that shoots fireballs with devastating effect.",
+      value: "25 gold",
+    },
+    {
+      name: "Cloak of Invisibility",
+      description: "A cloak that renders the wearer invisible to the naked eye.",
+      value: "75 gold",
+    },
+    // Add more items here
+];
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,88 @@
+import React, { useState, useEffect } from "react";
+
+const items = [
+  {
+    name: "Sword of the Undead",
+    description: "A cursed sword that drains the life force of its wielder.",
+    value: "50 gold",
+  },
+  {
+    name: "Wand of Fireball",
+    description: "A wand that shoots fireballs with devastating effect.",
+    value: "25 gold",
+  },
+  {
+    name: "Cloak of Invisibility",
+    description: "A cloak that renders the wearer invisible to the naked eye.",
+    value: "75 gold",
+  },
+  // Add more items here
+];
+
 function Home() {
+  const [cooldown, setCooldown] = useState(0);
+  const [loot, setLoot] = useState([]);
+
+  const scavenge = () => {
+    if (cooldown === 0) {
+      const randomLoot = [];
+      for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * items.length);
+        const item = items[randomIndex];
+        randomLoot.push(item);
+      }
+      setLoot(randomLoot);
+      setCooldown(300); // 5 minutes cooldown (300 seconds)
+    }
+  };
+
+  const resetCooldown = () => {
+    setCooldown(0);
+    setLoot([]);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (cooldown > 0) {
+      interval = setInterval(() => {
+        setCooldown((prev) => prev - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [cooldown]);
+
+  const getCooldownText = () => {
+    const minutes = Math.floor(cooldown / 60);
+    const seconds = cooldown % 60;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutes}:${formattedSeconds}`;
+  };
+
   return (
     <div className="home">
       <h1>Welcome to my RPG Text Game</h1>
-      <button className="scavenge-button">Scavenge for Loot</button>
+      <button className="scavenge-button" onClick={scavenge} disabled={cooldown > 0}>
+        {cooldown > 0 ? `Cooldown: ${getCooldownText()}` : "Scavenge for Loot"}
+      </button>
       <button className="expedition-button">Go on an Expedition</button>
       <button className="jobs-button">View Available Jobs</button>
       <button className="room-button">Go to your Room</button>
       <button className="profile-button">View your Profile</button>
+      {loot.length > 0 && (
+        <div>
+          <h2>You found:</h2>
+          <ul>
+            {loot.map((item) => (
+              <li key={item.name}>
+                <strong>{item.name}</strong>: {item.description} (Value: {item.value})
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <button onClick={resetCooldown}>Test Reset Cooldown</button>
     </div>
   );
 }
